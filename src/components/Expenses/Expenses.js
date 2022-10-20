ad7109707224fc23ee108ff9c2138298b581e347
Manuel Vicent <manueldavid.v@thoughtworks.com>
2022-10-20 15:56:47 +0200

    Add state lifting in between components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,32 @@
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
-    const expenses = props.expenses.map(expense => <ExpenseItem
+    const expenses = props.expenses.map((expense, i) => <ExpenseItem
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
             />);
 
+    const [selectedFilterValue, setSelectedFilterValue] = useState('2020');
+
+    const filterChangeHandler = (filterValue) => {
+        setSelectedFilterValue(filterValue);
+    }
+
+    console.log('In Expenses.js');
+    console.log(selectedFilterValue);
+
     return (
-        <Card className="expenses">
-            {expenses}
-        </Card>
+      <Card className="expenses">
+        <div>
+          <ExpensesFilter selected={selectedFilterValue} onFilterChange={filterChangeHandler} />
+        </div>
+        {expenses}
+      </Card>
     );
 }
 
